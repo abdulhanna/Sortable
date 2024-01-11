58ea517befc4a5892653d5be332f606255afdac5
abdul hannan <abdul.hannan@plaxonic.com>
2024-01-11 11:08:30 +0530

    bundler

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,24 @@
+const path = require('path');
+
 module.exports = {
-    // other webpack configurations
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader'
-          }
-        }
-      ]
-    }
-  };
-  
+  mode: 'production', // or 'development'
+  entry: './src/index.js',  // Adjust the entry point if needed
+  output: {
+    filename: 'bundle.js',  // Adjust the output filename if needed
+    path: path.resolve(__dirname, 'dist'),  // Adjust the output path if needed
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],  // Add .jsx to the list of extensions
+  },
+};
